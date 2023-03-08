9bc4da8fbf77643101094c238f158fa5d4cd0deb
Connor <52870187+Waboba97@users.noreply.github.com>
2023-03-08 14:15:45 -0500

    Update returnPulling.php

--- returnPulling.php
+++ returnPulling.php
@@ -1,5 +1,19 @@
 <?php
 
-if(isset($_POST))
+if(isset($_POST['submit'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-    ?>
+
+
+}
+
+
+function validateEmail($email) {
+    if (empty($email))
+        return "Must Enter your email!";
+    else if (!preg_match("/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/", $email))
+        //email format: the regular email but it also accepts firstname.lastname@aaa.bbb.com
+        return "Invalid email!";
+}
+?>
