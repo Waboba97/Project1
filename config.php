cb38607668157d3e5a90303356197639edcd7fea
braxton203 <101836781+braxton203@users.noreply.github.com>
2023-03-08 13:03:24 -0500

    Config and Connection files

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+
+//Define some important constants:
+
+//Flag for site status:
+define('LIVE', FALSE);
+
+// Admin contact address:
+define('EMAIL', 'Insert Real Admin Email Address Here');
+
+// Site URL (base for all redirections):
+define ('BASE_URL', 'http://localhost/m7-9/case');
+
+// Location of the MySQL connection script:
+define ('MYSQL_CONNECT', 'mysql_connect.php');
+
+// Adjust the time zone for PHP 5.1 and greater:
+date_default_timezone_set ('US/Eastern');
+
+
+
+
+?>
