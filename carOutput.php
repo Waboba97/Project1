d0488dc4e98bb255490e68338c29483fdf16835a
Brian Butzen <34663708+Bbutzen@users.noreply.github.com>
2023-03-22 13:58:27 -0400

    Update carOutput.php

--- carOutput.php
+++ carOutput.php
@@ -20,14 +20,15 @@
         throw new PDOException($e->getMessage(), (int)$e->getCode());
     }
     $userID = $_SESSION['user_id'];
-    $usersCarsArray = array(array());//Does this work?
+    $usersCarsArray = array(array('cid' =>0,'year'=>'', 'make'=>'', 'model'=>'', 'price'=>0));//Does this work? can't have the same car I think though
     $CIDQuery = "SELECT CID, year FROM users_cars WHERE UID = '$userID';";
     try {
         $result = $pdo -> query($CIDQuery);
         while ($row = $result->fetch(PDO::FETCH_NUM)) {
             //not sure where to go from here
             foreach ($row as $value) {
-                $usersCarsArray[[$row[0]][$row[1]] = $value;//assign the year to that CID??
+                $usersCarsArray[$row][0] = $value;//Adding the cid??
+                $usersCarsArray[$row][1] = $value;//Adding the year??
             }
         }
         //need make, model, price pulled from cars table where the CID matches
