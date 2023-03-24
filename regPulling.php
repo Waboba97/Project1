f179fa770effc1844d9b83777e4d531c8e61159d
Connor <52870187+Waboba97@users.noreply.github.com>
2023-03-24 15:25:13 -0400

    Reg

--- regPulling.php
+++ regPulling.php
@@ -19,34 +19,33 @@ if(isset($_POST['query'])) {
 //check if the passwords match
     if ($password != $passwordConfirm) {
         $passwordError = "Passwords do not match!";
+        echo $passwordError;
     }
 
     if (empty($email))
-        return "Must Enter your email!";
+        echo "Must Enter your email!";
     else if (!preg_match("/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/", $email))
         //email format: the regular email but it also accepts firstname.lastname@aaa.bbb.com
-        return "Invalid email!";
-
-
-    //if the email is valid, check if it is already in the database
-    if (validateEmail($email) == null) {
+        echo "Invalid email!";
+    else
         $sql = "SELECT * FROM car_owners WHERE email = '$email'";
         $result = $pdo->query($sql);
         if ($result->rowCount() > 0) {
             $emailError = "Email already exists!";
+            echo $emailError;
         }
-    }
+
 
     //if there are no errors, insert the data into the database
     if ($passwordError == null && $emailError == null) {
+        //hash the password
         $hash = password_hash($password, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO car_owners (firstName, lastName, email, hash) VALUES ('{$fName}', '{$lName}', '{$email}', '{$hash}')";
+        $sql = "INSERT INTO car_owners (firstName, lastName, email, password) VALUES ('{$fName}', '{$lName}', '{$email}', '{$hash}')";
         if ($pdo->exec($sql)) {
             echo "New record created successfully";
         } else {
             echo "Error: ";
         }
     }
-
 }
 ?>
