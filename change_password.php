e646af064ffd7262005b3bb689faee6e8cd79c45
kdearl <126713904+kdearl@users.noreply.github.com>
2023-03-24 15:53:17 -0400

    Update change_password.php

--- change_password.php
+++ change_password.php
@@ -1,10 +1,10 @@
 <?php
 
-    require_once ('config.php');
     $page_title = 'Change Your Password';
     include ('header.html');
     require_once "mysql_connect.php";
 
+    $displayForm = TRUE;
 
     $email = $_POST['email'];
     $pw = $_POST['password'];
@@ -24,6 +24,7 @@
         echo "Your password for $email has been successfully updated to 'Pass'.";
     }
 
+    if ($displayForm){
 ?>
 <h1>Change Your Password</h1>
 <p>Enter your email address below and your password will be changed.</p>
@@ -39,5 +40,6 @@
 </form>
 
 <?php
+    }
     include ('footer.html');
 ?>
