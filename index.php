a4b2fecd38262de74df555165d37ec1bcab4d628
Brian Butzen <34663708+Bbutzen@users.noreply.github.com>
2023-03-25 16:26:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ $page_title = 'Welcome to RedBook';
 include ('header.php');
 
 // Welcome the user (by name if they are logged in):
-if (isset($_SESSION['user_id'])) {
-	$name = htmlspecialchars($_SESSION['name']);
+if (isset($_SESSION['email'])) {
+	$name = htmlspecialchars($_SESSION['email']);
 	echo "<h1>Welcome, $name! </h1>";
     ?>
     <a href="carSelection.php">
